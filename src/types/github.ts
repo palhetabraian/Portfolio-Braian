7ec72671ddcd7be5831f2a174ee32be1ce32d1af
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-08-04 22:09:51 -0300

    feat: adiciona tipos da API do GitHub

---
+++ src/types/github.ts
@@ -0,0 +1,26 @@
+export type GithubRepositoryResponse = {
+  id: number;
+  name: string;
+  html_url: string;
+  description: string | null;
+  fork: boolean;
+  archived: boolean;
+  stargazers_count: number;
+  forks_count: number;
+  topics: string[];
+  languages_url: string;
+  updated_at: string;
+};
+
+export type GithubLanguagesResponse = Record<string, number>;
+
+export type GithubProject = {
+  id: number;
+  name: string;
+  repositoryUrl: string;
+  description: string;
+  technologies: string[];
+  stars: number;
+  forks: number;
+  updatedAt: string;
+};
